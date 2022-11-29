696817007023a39c1b285236e9be714bca8b55a9
Rohan Phulkar <rohanphulkar936@gmail.com>
2022-11-29 22:54:26 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -91,11 +91,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':config("dbName"),
+      'USER':config("dbUser"),
+      'PASSWORD':config("dbPassword"),
+      'HOST':config("dbHost"),
+      'PORT':config("dbPort"),
+   }
 }
 
 # Password validation
