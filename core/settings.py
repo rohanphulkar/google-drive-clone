454a0eb5d742ee9aee00080bebd3e685e5325fb7
Rohan Phulkar <rohanphulkar936@gmail.com>
2022-11-29 22:49:12 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -80,17 +80,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config("dbName"),
+#         'USER': config("dbUser"),
+#         'PASSWORD': config("dbPassword"),
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config("dbName"),
-        'USER': config("dbUser"),
-        'PASSWORD': config("dbPassword"),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
